d1e078d84185b8a4d4d0463a75320ffc1cadeacc
LorenzoBonanni <lorenzopuntobonanni@gmail.com>
2025-11-13 18:26:59 +0100

    Added AdvancedRBC

--- scripts/main_rbc.py
+++ scripts/main_rbc.py
@@ -1,17 +1,17 @@
 import sys, os
 
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from CityLearn.citylearn.agents.base import Agent
 from CityLearn.citylearn.agents.rbc import BasicRBC, HourRBC, OptimizedRBC
 from CityLearn.citylearn.citylearn import CityLearnEnv, Building
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Core
 from typing import Mapping, Union
 
 import warnings; warnings.filterwarnings("ignore", category=UserWarning)
 from utils import *
-
 
 class CustomRBC(BasicRBC):
 
@@ -53,6 +53,64 @@ class CustomRBC(BasicRBC):
         # Imposta la mappa nella superclasse
         HourRBC.action_map.fset(self, action_map)
 
+class AdvancedRBC(Agent):
+    """
+    Advanced Rule-Based Controller (RBC) Agent with comfort band consideration.
+
+    Parameters
+    ----------
+    env: CityLearnEnv
+        CityLearn environment to perform control on.
+    band: float
+        Comfort band to try to satisfy. 
+
+    """
+    def __init__(self, env: CityLearnEnv, band: float=2.0, **kwargs):
+
+        # Init OptimizedRBC
+        super().__init__(env, **kwargs)
+
+        # Comfort band (+/-) to satisfy
+        self.comfort_band = band 
+
+    def predict(self, observations: List[List[float]], deterministic: bool = True) -> List[List[float]]:        
+
+        actions = []
+        for i, o in enumerate(observations):
+
+            # Available spaces
+            available_obs = self.observation_names[i]
+            available_act = self.action_names[i]
+            action = [0.0 for _ in range(len(available_act))]
+
+            # TODO add other observations if needed
+            # TODO implement more advanced RBC logic for each device
+
+            # Indoor temperature and setpoints
+            indoor_temp = o[available_obs.index('indoor_dry_bulb_temperature')]
+            cooling_setpoint = o[available_obs.index('indoor_dry_bulb_temperature_cooling_set_point')]
+
+
+            if 'cooling_device' in available_act:
+                # EXAMPLE LOGIC: Turn on cooling if indoor temp exceeds setpoint + comfort band
+                if indoor_temp > cooling_setpoint + self.comfort_band:
+                    action[available_act.index('cooling_device')] = 1.0  # Turn on cooling
+                else:
+                    action[available_act.index('cooling_device')] = 0.0  # Turn off cooling
+
+            if 'electrical_storage' in available_act:
+                pass
+
+            if 'dhw_storage' in available_act:
+                pass
+
+            actions.append(action)
+
+        # Return overwritten actions
+        self.actions = actions
+        return actions
+
+
 def run_simulation(agent, env):
     print("Starting simulation...")
     print(f"Agent: {agent.__class__.__name__}")
@@ -62,7 +120,7 @@ def run_simulation(agent, env):
         actions = agent.predict(observations)
         observations, reward, terminated, truncated, info = env.step(actions)
 
-    print("Simulation completed.")
+    print("Simulation completed.\n")
 
 def main(args):
 
@@ -75,7 +133,7 @@ def main(args):
     # Create CityLearn environment
     env_1 = CityLearnEnv(schema=schema, central_agent=True)
     env_2 = CityLearnEnv(schema=schema, central_agent=True)
-    agent = CustomRBC(env_1)
+    agent = AdvancedRBC(env_1)
     baseline_agent = OptimizedRBC(env_2)
 
     run_simulation(agent, env_1)
@@ -83,7 +141,7 @@ def main(args):
 
     # Compare results
     plot_district_kpis(
-        {'CustomRBC': env_1, 'OptimizedRBC': env_2},
+        {'AdvancedRBC': env_1, 'OptimizedRBC': env_2},
         base_path='imgs'
     )
 
